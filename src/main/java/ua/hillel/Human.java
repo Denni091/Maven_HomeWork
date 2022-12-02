b1574934d0efede8a5aaa40c97d3e10ebc0b9553
Denni <denistynkovan5@gmail.com>
2022-12-02 10:16:08 +0200

    First Commit

--- src/main/java/ua/hillel/Human.java
+++ src/main/java/ua/hillel/Human.java
@@ -1,2 +1,13 @@
-package ua.hillel;public class Human {
+package ua.hillel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+@Setter
+@Getter
+@AllArgsConstructor
+public class Human {
+
+    private String name;
+    private String surName;
+    private int age;
 }
--- src/main/java/ua/hillel/Main.java
+++ src/main/java/ua/hillel/Main.java
@@ -2,6 +2,8 @@ package ua.hillel;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Human human = new Human("Tom", "Davidson", 22);
+        System.out.println("Name: " + human.getName() +
+                "\nSurname: " + human.getSurName() + "\nAge: " + human.getAge());
     }
 }
